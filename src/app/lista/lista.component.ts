568e73f67735974bde487d03499eeddc436c2772
El4ine13 <elainemkx@gmail.com>
2024-10-06 23:30:19 -0300

    commit lista

---
+++ src/app/lista/lista.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css'],
+  standalone: true,
+  imports: [FormsModule, CommonModule]
+})
+export class ListaComponent {
+  novoItem: string = '';
+  listaCompras: { nome: string, comprado: boolean }[] = [
+    { nome: 'Maçã', comprado: false },
+    { nome: 'Arroz', comprado: false },
+    { nome: 'Leite', comprado: true },
+  ];
+
+  marcarComoComprado(item: { nome: string, comprado: boolean }) {
+    item.comprado = !item.comprado;
+  }
+
+  adicionarItem(nome: string) {
+    if (nome) {
+      this.listaCompras.push({ nome, comprado: false });
+      this.novoItem = ''; 
+    }
+  }
+
+  removerItem(index: number) {
+    this.listaCompras.splice(index, 1);
+  }
+}
